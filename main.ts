15c817de4458ee53b3b2c95404de867d95b6eb4a
MrEuclid <john@teacherjohn.org>
2024-08-24 15:02:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    total += 1
+    helmet += 1
 })
+input.onButtonPressed(Button.B, function () {
+    total += 1
+    no_helmet += 1
+})
+let total = 0
+let helmet = 0
+let no_helmet = 0
