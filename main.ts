effea309ffc1223624ea14661e97ea70b3200076
MrEuclid <john@teacherjohn.org>
2024-08-24 17:30:54 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,33 @@
 input.onButtonPressed(Button.A, function () {
     total += 1
     helmet += 1
+    basic.showNumber(1)
+    basic.pause(200)
+    basic.clearScreen()
+    datalogger.log(
+    datalogger.createCV("a", helmet),
+    datalogger.createCV("t", no_helmet),
+    datalogger.createCV("b", total)
+    )
 })
 input.onButtonPressed(Button.B, function () {
     total += 1
     no_helmet += 1
+    basic.showNumber(0)
+    basic.pause(200)
+    basic.clearScreen()
+    datalogger.log(
+    datalogger.createCV("a", helmet),
+    datalogger.createCV("t", no_helmet),
+    datalogger.createCV("b", total)
+    )
 })
-let total = 0
-let helmet = 0
 let no_helmet = 0
+let helmet = 0
+let total = 0
+basic.showString("H")
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
+datalogger.mirrorToSerial(true)
+total = 0
+helmet = 0
+no_helmet = 0
